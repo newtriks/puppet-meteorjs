37b3206631078dc42f0710d4ecb2c89f857f3a57
Simon Bailey <simon@newtriks.com>
2013-04-06 16:10:35 +0100

    Removed -O from curl command
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class meteorjs (
 	include boxen::config
 
     exec { 'install meteorjs':
-		command => "curl -O ${url} && chmod a+x meteorjs",
+		command => "curl ${url} && chmod a+x meteorjs",
 		creates => "${boxen::config::bindir}/meteorjs",
 		cwd     => $boxen::config::bindir,
     }
--- spec/classes/meteorjs_spec.rb
+++ spec/classes/meteorjs_spec.rb
@@ -6,7 +6,7 @@ describe "meteorjs" do
   it do
     should include_class("boxen::config")
     should contain_exec("install meteorjs").with({
-      :command => "curl -O https://install.meteor.com && chmod a+x meteorjs",
+      :command => "curl https://install.meteor.com && chmod a+x meteorjs",
       :creates => "/test/boxen/bin/meteorjs",
       :cwd     => "/test/boxen/bin"
     })
